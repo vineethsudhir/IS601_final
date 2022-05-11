26ae059dacc3995863feea181ac6cc10e5f7f5d7
vineethsudhir <vineeth.sudhir@gmail.com>
2022-05-11 12:14:20 -0400

    Authentication test added

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,14 +1,16 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
-    """This makes the index page"""
+    """This tests the home page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
+
 def test_auth_pages(client):
-    """This makes the index page"""
+    """This tests the home page"""
     response = client.get("/dashboard")
     assert response.status_code == 302
     response = client.get("/register")
@@ -16,7 +18,40 @@ def test_auth_pages(client):
     response = client.get("/login")
     assert response.status_code == 200
 
-def test_csv_upload(client):
-    """This tests csv upload without login"""
-    response = client.get("/transactions/upload")
+
+def test_user_menu_links(client):
+    """This tests the user links , without login"""
+    response = client.get("/profile")
     assert response.status_code == 302
+    response = client.get("/account")
+    assert response.status_code == 302
+    response = client.get("/logout")
+    assert response.status_code == 302
+    response = client.get("/users")
+    assert response.status_code == 302
+
+
+def test_user_menu_links_after_login(client):
+    with client:
+        """This tests the user menu links , after login"""
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+        login_response = client.post("/login", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test"
+        },
+                                     follow_redirects=True)
+
+        assert login_response.status_code == 200
+
+        response = client.get("/profile")
+        assert response.status_code == 200
+        response = client.get("/account")
+        assert response.status_code == 200
+        response = client.get("/logout")
+        assert response.status_code == 302
+        assert b'href="/login"' in response.data
